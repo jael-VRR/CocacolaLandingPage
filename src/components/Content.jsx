2212700e800a2f8045a291bce025877d27c8c2e9
jael-VRR <jrodriguezruidias@gmail.com>
2021-02-05 15:19:37 -0500

    add content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import '../styles/Content.css';
 
-function content() {
-    return (
-        <div>
-            
-        </div>
-    )
+
+function Content() {
+  return (
+    <div className="content-text">
+      <h2>Taste de feeling</h2>
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </p>
+      <i className="link">Learn More</i>
+    </div>
+  );
 }
 
-export default content
+export default Content;
