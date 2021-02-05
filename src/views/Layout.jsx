c93f447fd5ad2e06378874d87b1772b709abfb90
jael-VRR <jrodriguezruidias@gmail.com>
2021-02-05 15:21:00 -0500

    modifying layout

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import Navbar from '../components/Navbar.jsx'
+import { Row, Col } from "react-bootstrap";
+import Navbar from '../components/Navbar.jsx';
+import Content from '../components/Content.jsx';
+import Slider from '../styles/Slider.css';
 import '../styles/Layout.css';
 
 const Layout = () => {
@@ -9,8 +12,12 @@ const Layout = () => {
       <header>
         <Navbar/>
       </header>
-        
-      
+      <div>
+      <Content/>
+      </div>
+      <div>
+        <Slider/>
+      </div>
     </section>
   )
 }
