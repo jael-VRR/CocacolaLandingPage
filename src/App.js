7088dbe6c5b475ece378fe3ca630a0cc5d19afe3
jael-VRR <jrodriguezruidias@gmail.com>
2021-02-06 00:07:34 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/layout">
+        <Route exact path="/">
           <Layout />
         </Route>
       </Switch>
