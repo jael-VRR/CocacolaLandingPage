1563e4e4aabdce8415556fb4608e03e889cb8ae6
jael-VRR <jrodriguezruidias@gmail.com>
2021-02-05 23:35:02 -0500

    modifying slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 import Swiper from "react-id-swiper";
-import '../styles/Slider.css';
+import "../styles/Slider.css";
 import cocacola1 from "../images/cocacola1.png";
 import cocacola2 from "../images/cocacola2.png";
 import cocacola3 from "../images/cocacola3.png";
@@ -10,40 +10,46 @@ import cocacola6 from "../images/cocacola6.png";
 
 function Slider() {
   const params = {
-    effect: "cube",
+    effect: "coverflow",
     grabCursor: true,
-    cubeEffect: {
-      shadow: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 200,
+      modifier: 1,
       slideShadows: true,
-      shadowOffset: 20,
-      shadowScale: 0.94,
     },
     pagination: {
       el: ".swiper-pagination",
     },
+    loop: true,
   };
   return (
-    <div className="content-slide">
-      <Swiper {...params}>
-        <div>
-          <img className="item" src={cocacola1} alt="cocacola1" />
-        </div>
-        <div>
-          <img className="item" src={cocacola2} alt="cocacola2" />
-        </div>
-        <div>
-          <img className="item" src={cocacola3} alt="cocacola3" />
-        </div>
-        <div>
-          <img className="item" src={cocacola4} alt="cocacola4" />
-        </div>
-        <div>
-          <img className="item" src={cocacola5} alt="cocacola5" />
-        </div>
-        <div>
-          <img className="item" src={cocacola6} alt="cocacola6" />
-        </div>
-      </Swiper>
+    <div className="imgBox">
+      <div className="swipper-container">
+        <Swiper {...params}>
+          <div>
+            <img className="swiper-slide" src={cocacola1} alt="cocacola1" />
+          </div>
+          <div>
+            <img className="swiper-slide" src={cocacola2} alt="cocacola2" />
+          </div>
+          <div>
+            <img className="swiper-slide" src={cocacola3} alt="cocacola3" />
+          </div>
+          <div>
+            <img className="swiper-slide" src={cocacola4} alt="cocacola4" />
+          </div>
+          <div>
+            <img className="swiper-slide" src={cocacola5} alt="cocacola5" />
+          </div>
+          <div>
+            <img className="swiper-slide" src={cocacola6} alt="cocacola6" />
+          </div>
+        </Swiper>
+      </div>
     </div>
   );
 }
